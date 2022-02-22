e16349acce3e1f891c37f524c91b74332aa06adf
Edouard <support@diskloud.fr>
2022-02-22 21:08:41 +0100

    First content inside mathy.js

---
+++ mathy.js
@@ -0,0 +1,29 @@
+/*
+Mathy Project (2022)
+This project is protected by the MIT License
+Edouard Aubert
+*/
+
+function assert(condition,errorMessage="One test did not pass") {
+    if (!condition)
+        throw errorMessage
+}
+
+function parse(input) {
+    subFirst=[];
+    lev=-1;
+    for (var i=0;i<input.length;i++) {
+        if (input[i]=="(") {
+            subFirst.push(i);
+            lev+=1;
+        } else if (input[i]==")") {
+            if (lev==-1) {
+                return "Invalid expression";
+            } else {
+                //parse(input.slice(subFirst,i-subFirst))
+                console.log(subFirst);
+                lev-=1;
+            }
+        }
+    }
+}
